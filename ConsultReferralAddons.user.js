fcb8a08b73ed32cb206bdce51b03401a2cf4aab8
sggongshow <87620031+sggongshow@users.noreply.github.com>
2023-07-24 21:13:47 -0700

    Update ConsultReferralAddons.user.js

--- ConsultReferralAddons.user.js
+++ ConsultReferralAddons.user.js
@@ -3,12 +3,13 @@
 // @namespace   GongOscar
 // @description Consult Referal Addons
 // @include     */ConsultationFormRequest.jsp*
-// @include     *ViewRequest.do?*
 // @require     https://code.jquery.com/jquery-3.6.0.js
 // @grant       GM_addStyle
-// @version			23.07.21.0
+// @version			23.07.24.1
 // ==/UserScript==
 
+
+//Changelog 23.07.24.1 - Added semicolon spacing button
 
 window.addEventListener('load', function() {
 
@@ -20,12 +21,21 @@ window.addEventListener('load', function() {
   faxNoteBut.onclick = faxNoteButFunc
   faxNoteBut.setAttribute('style', 'width:120px;font-size:12px;padding:0px; background-color:yellow;');
 
+  var semiColonBut = document.createElement('input');
+  semiColonBut.type = 'button';
+  semiColonBut.id = 'semiColonBut'
+  semiColonBut.name = 'semiColonBut'
+  semiColonBut.value = 'SemiColon Spacing'
+  semiColonBut.onclick = semiColonSpacing
+  semiColonBut.setAttribute('style', 'width:120px;font-size:12px;padding:0px; background-color:cyan;');
+
   var appendBox = document.getElementsByClassName("container-fluid well")[0].firstElementChild.firstElementChild
   var childElements = appendBox.children
   appendBox = childElements[childElements.length-3]
 
 
   appendBox.firstElementChild.appendChild(faxNoteBut);
+  appendBox.firstElementChild.appendChild(semiColonBut);
 
 
 
@@ -51,6 +61,21 @@ function faxNoteButFunc(){
 
   // Pending specialist clicked
   document.getElementsByClassName("stat")[4].firstElementChild.click()
+
+
+}
+
+function semiColonSpacing(){
+  var clinicalInfoBox = document.getElementById("clinicalInformation")
+  var sigProblemsBox = document.getElementById("concurrentProblems")
+  var currentMedsBox = document.getElementById("currentMedications")
+
+  var newclincalInfoText = clinicalInfoBox.value.replace(/;/g, ";\n")
+  clinicalInfoBox.value = newclincalInfoText
+  var newsigProblemsBox = sigProblemsBox.value.replace(/;/g, ";\n")
+  sigProblemsBox.value = newsigProblemsBox
+  var newcurrentMedsBox = currentMedsBox.value.replace(/;/g, ";\n")
+  currentMedsBox.value = newcurrentMedsBox
 
 
 }
