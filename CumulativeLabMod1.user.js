b1ea627ad905e0dd9740de0505b6d56478392b27
sggongshow <87620031+sggongshow@users.noreply.github.com>
2025-01-14 09:25:57 -0800

    Update CumulativeLabMod1.user.js

--- CumulativeLabMod1.user.js
+++ CumulativeLabMod1.user.js
@@ -4,12 +4,12 @@
 // @include     *lab/CumulativeLabValues.jsp*
 // @require     https://code.jquery.com/jquery-3.6.0.js
 // @grant       GM_addStyle
-// @version 	24.11.12.0
+// @version 	25.01.14.0
 // ==/UserScript==
 //========Get Path============
 
 //Changelog
-//24.11.12.0 - added more to removeUnwanted
+//25.01.14.0 - added more to removeUnwanted
 //24.06.13.0 - added more to removeUnwanted
 //24.04.09.0 - added more to removeUnwanted
 //24.02.23.0 - added more to removeUnwanted
@@ -450,7 +450,7 @@ function removeUnwanted() {
 				     'holter', 'mammo', 'other','colonoscopy','pathology', 'note', 'progress',
                            	     'surgical','operation', 'discharge','date','referred','ecg', 'egd',
 				     'consult', 'scopy', 'scope', 'summary', 'treat', 'med', 'exam', 'review',
-				     'cervix', 'clnic' ] //'exam'
+				     'cervix', 'provider', 'clnic' ] //'exam'
       for (i = myLabArray.length -1  ; i >= 0 ; i--) {
 				var toDelete = 0
     		for (j = 0; j < unwantedWords.length; j++) {
